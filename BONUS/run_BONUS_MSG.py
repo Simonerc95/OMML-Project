f8bf41bbe3a433e749df0a21d8e7154c6facc045
Simonerc95 <s.ercolino995@gmail.com>
2020-11-23 12:39:14 +0100

    Bonus

---
+++ BONUS/run_BONUS_MSG.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 22 17:43:26 2020
+
+@author: m.broglio
+"""
+
+from functions_BONUS_MSG import *
+np.random.seed(1679838)
+df = pd.read_csv(r'..\OMML2020_Assignment_1_Dataset.csv')
+path = r'.\DataPointsBonusTest.csv'  # please, put here path of the bonus test dataset
+test_dataset = pd.read_csv(path)
+
+
+with open('best_model_bonus.pkl', 'rb') as pkl:
+    best_model = pickle.load(pkl)
+
+
+best_model.print_params()
+best_model.predict_and_rmse_base(df)
+best_model.predict_and_rmse_test(test_dataset)
+get_plot(best_model, df)
